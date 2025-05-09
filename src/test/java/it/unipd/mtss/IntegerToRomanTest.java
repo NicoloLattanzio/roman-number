0856947c3d02e2d82f48d7cc75620553b03ed911
Andrea Sgreva <6190216@itisrossi.vi.it>
2025-05-09 13:55:36 +0200

    implementazione test primi 3 numeri close #2

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -3,6 +3,38 @@
 // Andrea Sgreva 2113179
 /////////////////////////////////////////////////
 package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IntegerToRomanTest {
-    
+    @Test
+    void convertShouldReturnIWhenNumberIs1() {
+        assertEquals("I", IntegerToRoman.convert(1));
+    }
+
+    @Test
+    void convertShouldReturnIIWhenNumberIs2() {
+        assertEquals("II", IntegerToRoman.convert(2));
+    }
+
+    @Test
+    void convertShouldReturnIIIWhenNumberIs3() {
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+
+    @Test
+    void convertShouldThrowExceptionWhenNumberIsZero() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(0));
+    }
+
+    @Test
+    void convertShouldThrowExceptionWhenNumberIsNegative() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(-1));
+    }
+
+    @Test
+    void convertShouldThrowExceptionWhenNumberIsAbove3() {
+        assertThrows(IllegalArgumentException.class, () -> IntegerToRoman.convert(4));
+    }
 }
--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -3,6 +3,59 @@
 // Andrea Sgreva 2113179
 /////////////////////////////////////////////////
 package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class RomanPrinterTest {
-    
+    @Test
+    public void testPrintI() {
+        String expected = 
+            " _____ \n" +
+            "|_   _|\n" +
+            "  | |  \n" +
+            "  | |  \n" +
+            " _| |_ \n" +
+            "|_____|\n";
+        assertEquals(expected, RomanPrinter.print(1));
+    }
+
+    @Test
+    public void testPrintII() {
+        String expected = 
+            " _____   _____ \n" +
+            "|_   _| |_   _|\n" +
+            "  | |     | |  \n" +
+            "  | |     | |  \n" +
+            " _| |_   _| |_ \n" +
+            "|_____| |_____|\n";
+        assertEquals(expected, RomanPrinter.print(2));
+    }
+
+    @Test
+    public void testPrintIII() {
+        String expected = 
+            " _____   _____   _____ \n" +
+            "|_   _| |_   _| |_   _|\n" +
+            "  | |     | |     | |  \n" +
+            "  | |     | |     | |  \n" +
+            " _| |_   _| |_   _| |_ \n" +
+            "|_____| |_____| |_____|\n";
+        assertEquals(expected, RomanPrinter.print(3));
+    }
+
+    @Test
+    public void testPrintEmptyForZero() {
+        assertEquals("", RomanPrinter.print(0));
+    }
+
+    @Test
+    public void testPrintEmptyForNegative() {
+        assertEquals("", RomanPrinter.print(-1));
+    }
+
+    @Test
+    public void testPrintEmptyForAbove3() {
+        assertEquals("", RomanPrinter.print(4));
+    }
 }
